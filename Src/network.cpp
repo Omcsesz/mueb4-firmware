d5ca3b1a3ab7d44b7a93b3925041f89757dd86d3
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2020-03-07 20:11:28 +0100

    Remove redundant bit shifts
    The correct color value can be read without 1 left shift

--- Src/network.cpp
+++ Src/network.cpp
@@ -143,45 +143,45 @@ namespace{
         
         uint8_t r, g, b;
 
-        r = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        r = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         g = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        b = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        b = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         first_window.pixels[0].set(r, g, b);
 
         r = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        g = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        g = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         b = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
         first_window.pixels[1].set(r, g, b);
 
-        r = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        r = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         g = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        b = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        b = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         first_window.pixels[2].set(r, g, b);
 
         r = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        g = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        g = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         b = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
         first_window.pixels[3].set(r, g, b);
 
         //---------------------------------
 
-        r = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        r = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         g = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        b = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        b = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         second_window.pixels[0].set(r, g, b);
 
         r = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        g = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        g = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         b = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
         second_window.pixels[1].set(r, g, b);
 
-        r = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        r = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         g = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        b = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        b = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         second_window.pixels[2].set(r, g, b);
 
         r = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
-        g = (buff[ (base_offset+running_offset)  ]  & 0xf0) << 1;
+        g = (buff[ (base_offset+running_offset)  ]  & 0xf0);
         b = (buff[ (base_offset+running_offset++)]  & 0x0f) << 5;
         second_window.pixels[3].set(r, g, b);
     }
