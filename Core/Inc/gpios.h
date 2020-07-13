ac1fb3b41450065987115577cea00074d662ca2a
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2020-07-13 12:32:32 +0200

    Refactor gpios.h

--- Core/Inc/gpios.h
+++ Core/Inc/gpios.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#include "stm32f0xx_ll_gpio.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
 
 /// Output
 
@@ -27,30 +31,6 @@ void set_gpio(enum output_gpioes);
 void toogle_gpio(enum output_gpioes);
 #define toogle_gpio(x) LL_GPIO_TogglePin(x##_GPIO_Port, x##_Pin);
 
-// These are the output of cubeMx, also the inputs of ST functions. Cannot throw
-// them away :(
-
-#define WINDOW_POWER_LEFT_Pin LL_GPIO_PIN_13
-#define WINDOW_POWER_LEFT_GPIO_Port GPIOC
-#define SPI1_NSS_Pin LL_GPIO_PIN_4
-#define SPI1_NSS_GPIO_Port GPIOA
-#define W5500_RESET_Pin LL_GPIO_PIN_1
-#define W5500_RESET_GPIO_Port GPIOB
-#define LED_JOKER_Pin LL_GPIO_PIN_12
-#define LED_JOKER_GPIO_Port GPIOB
-#define LED_COMM_Pin LL_GPIO_PIN_13
-#define LED_COMM_GPIO_Port GPIOB
-#define LED_DHCP_Pin LL_GPIO_PIN_14
-#define LED_DHCP_GPIO_Port GPIOB
-#define LED_HEARTH_Pin LL_GPIO_PIN_15
-#define LED_HEARTH_GPIO_Port GPIOB
-#define WINDOW_POWER_RIGHT_Pin LL_GPIO_PIN_11
-#define WINDOW_POWER_RIGHT_GPIO_Port GPIOA
-#define WINDOW_3V3_RIGHT_Pin LL_GPIO_PIN_8
-#define WINDOW_3V3_RIGHT_GPIO_Port GPIOB
-#define WINDOW_3V3_LEFT_Pin LL_GPIO_PIN_9
-#define WINDOW_3V3_LEFT_GPIO_Port GPIOB
-
 /// Input
 
 enum input_gpioes { USER_INPUT_BUTTON };
@@ -58,5 +38,6 @@ enum input_gpioes { USER_INPUT_BUTTON };
 uint32_t read_gpio(enum input_gpioes);
 #define read_gpio(x) LL_GPIO_IsInputPinSet(x##_GPIO_Port, x##_Pin);
 
-#define USER_INPUT_BUTTON_Pin LL_GPIO_PIN_2
-#define USER_INPUT_BUTTON_GPIO_Port GPIOB
+#ifdef __cplusplus
+}
+#endif
