ee35027a9355404ed34a9f9e8a4581cf69815b7c
Zsombor Bodnár <zsombor.bodnar@kszk.bme.hu>
2021-06-21 00:53:23 +0200

    SM-39

--- Core/Src/firmware_updater.cc
+++ Core/Src/firmware_updater.cc
@@ -17,7 +17,7 @@
 extern std::uintptr_t _main_program_pages[];
 extern std::uintptr_t _main_program_start[];
 
-extern SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi1;
 
 /// WIZnet chip select
 void CsSel() {
@@ -62,6 +62,21 @@ extern "C" void FirmwareUpdater() {
   reg_wizchip_cs_cbfunc(CsSel, CsDesel);
   reg_wizchip_spi_cbfunc(SpiRb, SpiWb);
   reg_wizchip_spiburst_cbfunc(SpiRBurst, SpiWBurst);
+
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
 
   // For program and erase operations on the Flash memory (write/erase), the
   // internal RC oscillator (HSI) must be ON.
@@ -127,7 +142,10 @@ extern "C" void FirmwareUpdater() {
     }
 
     getsockopt(3, SO_STATUS, &status);
-  } while (status != SOCK_CLOSED);
+  } while (getSn_RX_RSR(3) != 0 || status != SOCK_CLOSE_WAIT);
+
+  disconnect(3);
+  close(3);
 
   HAL_NVIC_SystemReset();
 }
